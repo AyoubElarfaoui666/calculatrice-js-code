6290db43e65f2c1daa1073725261c421d5a3be7c
AyoubElarfaoui666 <ayoubElarfaoui@proton.me>
2025-08-21 12:36:18 +0000

    project

--- src/main.js
+++ src/main.js
@@ -1,31 +1,21 @@
-let time = document.querySelector(".time") ;
-let btnStart = document.querySelector(".start") ;
-let btnStop = document.querySelector(".stop") ;
-let btnReset = document.querySelector(".reset") ; 
 
-let count = 0 ;
-let clearLoop ;
+let seconds = document.querySelector(".seconds") ;
+let minutes = document.querySelector(".minutes") ;
+let hours = document.querySelector(".hours") ;
 
-btnStart.onclick = () => {
-    clearLoop = setInterval(() => {
-        count++ ;
-        let second = count%60 ;
-        let minutes = Math.floor(count/60) ;
-        let hour = Math.floor(minutes/60 ) ;
-        time.innerHTML = `${hour <= 9 ? "0"+hour : hour} :  ${minutes <= 9 ? "0"+minutes : minutes} : ${second <= 9 ? "0"+second : second}`
-    },1000) ;
-}
-
-btnStop.onclick = () => {
-    clearInterval(clearLoop) ;
-}
-
-btnReset.onclick = () => {
-    time.innerHTML = `00 : 00 : 00` ;
-    count = 0 ;
-}
-
-
+setInterval(() => {
+    let time = new Date() ;
+    console.log(time)
+    let second = time.getSeconds() ;
+    let minute = time.getMinutes() ;
+    let hour = time.getHours() ;
+    let secondDeg = second * 6 ;
+    let minuteDeg = minute * 6 ;
+    let hourDeg = hour * 30 + minute * 0.5 ;
+    seconds.style.cssText = `transform : rotate(${secondDeg}deg)` ;
+    minutes.style.cssText = `transform : rotate(${minuteDeg}deg)` ;
+    hours.style.cssText = `transform : rotate(${hourDeg}deg)` ;
+},1000)
 
 
 
