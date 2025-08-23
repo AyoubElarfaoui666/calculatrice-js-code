93e3566746a44338df24859e04f611ddc55b4d79
AyoubElarfaoui666 <ayoubElarfaoui@proton.me>
2025-08-23 12:35:18 +0000

    project

--- src/main.js
+++ src/main.js
@@ -1,34 +1,28 @@
+let result = document.querySelector(".result") ;
+let buttons = document.querySelectorAll(".numbers div") ;
 
-let seconds = document.querySelector(".seconds") ;
-let minutes = document.querySelector(".minutes") ;
-let hours = document.querySelector(".hours") ;
-
-setInterval(() => {
-    let time = new Date() ;
-    console.log(time)
-    let second = time.getSeconds() ;
-    let minute = time.getMinutes() ;
-    let hour = time.getHours() ;
-    let secondDeg = second * 6 ;
-    let minuteDeg = minute * 6 ;
-    let hourDeg = hour * 30 + minute * 0.5 ;
-    seconds.style.cssText = `transform : rotate(${secondDeg}deg)` ;
-    minutes.style.cssText = `transform : rotate(${minuteDeg}deg)` ;
-    hours.style.cssText = `transform : rotate(${hourDeg}deg)` ;
-},1000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Array.from(buttons).forEach((ele,index) => {
+    ele.onclick = (e) => {
+        let val = e.target.innerHTML ;
+        if(val === "AC"){
+            result.innerHTML = "0" ;
+        }else if(val === "DEL" ){
+            let arr = result.innerHTML.split("").slice(0,-1) ;
+            result.innerHTML = arr.join("") ;
+        }else if(val === "="){
+            try{
+                result.innerHTML = eval(result.innerHTML) ;
+            }catch(err){
+                result.innerHTML = err ;
+            }
+        }else{
+            if(result.innerHTML.length <14){
+                if(result.innerHTML !== "0"){
+                    result.append(val) ;
+                }else{
+                    result.innerHTML = val ;
+                }
+            }
+        }
+    }
+})
